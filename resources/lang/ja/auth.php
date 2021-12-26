f1b1ceb6443b87742fa77517cf6c92f4097bb5d1
miguel-aoyama <aoyamarikiya@gmail.com>
2021-12-26 18:07:48 +0900

    change validation lang

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -2,19 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'ログインできません。入力した情報に誤りがないかご確認ください。',
+    'throttle' => '何度もログインに失敗したため、:seconds秒後に再度お試しください。',
 
 ];
